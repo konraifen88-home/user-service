2f21caa93bf781fb347b0462df385e16f3050acb
konraifen88 <konraifen88@googlemail.com>
2018-08-21 06:58:07 +0200

    Add the cloud configuration and a start/remove script for cf apps

--- src/main/java/de/konraifen88/home/user/UserApplication.java
+++ src/main/java/de/konraifen88/home/user/UserApplication.java
@@ -19,10 +19,12 @@ package de.konraifen88.home.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
 
     public static void main(String[] args) {
